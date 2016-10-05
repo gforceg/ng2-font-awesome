48859fe841aa72f6ebedc12e1227c821bb8cf01e
action jackson <action@localhost.localdomain>
2016-10-05 16:39:29 -0600

    sanitize faIcon input via a setter property. (allows for changing the faIcon at runtime.)

--- src/component/font-awesome.component.ts
+++ src/component/font-awesome.component.ts
@@ -9,18 +9,22 @@ import { Component, OnInit, Input } from '@angular/core';
   // styleUrls: ['font-awesome.component.css']
 })
 export class FontAwesomeComponent implements OnInit {
-  @Input() faIcon: string // font awesome icons
-
-  // ngOnInit sanitizes faIcon on init
-  ngOnInit() {
-    // add 'fa-' to the faIcon if it isn't already present
-    if (this.faIcon) {
-      // if they didn't provide something like this: 'fa fa-cubes', fix it for them.
-      if (!this.faIcon.match(/^\s?fa fa\-/i)) {
-        if (!this.faIcon.match(/^fa\-/i)) { this.faIcon = 'fa-' + this.faIcon; }
-        if (!this.faIcon.match(/^fa /i)) { this.faIcon = 'fa ' + this.faIcon; }
+  private _faIcon: string // font awesome icons
+  @Input() set faIcon(icon: string) {
+    // sanitize the icon each time it is set.
+    // if they didn't provide something like this: 'fa fa-cubes', fix it for them.
+    if (icon) {
+      if (!icon.match(/^\s?fa fa\-/i)) {
+        if (!icon.match(/^fa\-/i)) { icon = 'fa-' + icon; }
+        if (!icon.match(/^fa /i)) { icon = 'fa ' + icon; }
       }
     }
+    this._faIcon = icon;
+  }
+
+  get faIcon() { return this._faIcon; }
+  // ngOnInit sanitizes faIcon on init
+  ngOnInit() {
   }
 }
 
