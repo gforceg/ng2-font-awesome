65ca824441854aa6504ef6dfee01731a781e37e1
action jackson <action@localhost.localdomain>
2016-10-25 12:32:44 -0600

    AOT compilation incr ver

--- gulp-tasks/todo.js
+++ gulp-tasks/todo.js
@@ -4,7 +4,7 @@ let gutil = require('gulp-util');
 var todo = require('gulp-todo');
 
 gulp.task('todo', () => {
-  gulp.src('**/*.{ts,html,scss}')
+  gulp.src(['**/*.{ts,html,scss}', 'gulpfile.js'])
   .pipe(todo())
   .pipe(gulp.dest('./'))
 });
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ let runSequence = require('run-sequence');
 
 require('require-dir')('./gulp-tasks');
 
+// todo: add a AOT compilation task
+// execSync: 'node_modules/.bin/ngc -p tsconfig-aot.json' ?
 gulp.task('default', (done) => {
   runSequence(
     'todo',
