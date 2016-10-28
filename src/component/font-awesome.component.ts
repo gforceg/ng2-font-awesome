5af38bdf29f690da04ec1d8cdf823fdb2abe8d3e
action jackson <action@localhost.localdomain>
2016-10-28 14:32:27 -0600

    stop using typescript getters / setters as an AoT test.

--- src/component/font-awesome.component.ts
+++ src/component/font-awesome.component.ts
@@ -6,20 +6,21 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['font-awesome.component.css']
 })
 export class FontAwesomeComponent implements OnInit {
-  private _faIcon: string // font awesome icons
-  @Input() set faIcon(icon: string) {
-    // sanitize the icon each time it is set.
-    // if they didn't provide something like this: 'fa fa-cubes', fix it for them.
-    if (icon) {
-      if (!icon.match(/^\s?fa fa\-/i)) {
-        if (!icon.match(/^fa\-/i)) { icon = 'fa-' + icon; }
-        if (!icon.match(/^fa /i)) { icon = 'fa ' + icon; }
-      }
-    }
-    this._faIcon = icon;
-  }
+  @Input() faIcon: string;
+  // private _faIcon: string // font awesome icons
+  // @Input() set faIcon(icon: string) {
+  //   // sanitize the icon each time it is set.
+  //   // if they didn't provide something like this: 'fa fa-cubes', fix it for them.
+  //   if (icon) {
+  //     if (!icon.match(/^\s?fa fa\-/i)) {
+  //       if (!icon.match(/^fa\-/i)) { icon = 'fa-' + icon; }
+  //       if (!icon.match(/^fa /i)) { icon = 'fa ' + icon; }
+  //     }
+  //   }
+  //   this._faIcon = icon;
+  // }
 
-  get faIcon() { return this._faIcon; }
+  // get faIcon() { return this._faIcon; }
   // ngOnInit sanitizes faIcon on init
   ngOnInit() {
   }
