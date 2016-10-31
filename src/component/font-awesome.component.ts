72a36f6e6899190504010b6d44b4b8de2043c84e
action jackson <action@localhost.localdomain>
2016-10-31 11:09:20 -0600

    brought font-awesome back to its original form.

--- src/component/font-awesome.component.ts
+++ src/component/font-awesome.component.ts
@@ -7,23 +7,22 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FontAwesomeComponent implements OnInit {
   // @Input() faIcon: string;
-  public faIcon: string = 'fa fa-cubes';
   // the following is commented out while I troubleshoot AoT...
   // but I believe properties are still acceptable for static analysis...
-  // private _faIcon: string // font awesome icons
-  // @Input() set faIcon(icon: string) {
-  //   // sanitize the icon each time it is set.
-  //   // if they didn't provide something like this: 'fa fa-cubes', fix it for them.
-  //   if (icon) {
-  //     if (!icon.match(/^\s?fa fa\-/i)) {
-  //       if (!icon.match(/^fa\-/i)) { icon = 'fa-' + icon; }
-  //       if (!icon.match(/^fa /i)) { icon = 'fa ' + icon; }
-  //     }
-  //   }
-  //   this._faIcon = icon;
-  // }
+  private _faIcon: string // font awesome icons
+  @Input() set faIcon(icon: string) {
+    // sanitize the icon each time it is set.
+    // if they didn't provide something like this: 'fa fa-cubes', fix it for them.
+    if (icon) {
+      if (!icon.match(/^\s?fa fa\-/i)) {
+        if (!icon.match(/^fa\-/i)) { icon = 'fa-' + icon; }
+        if (!icon.match(/^fa /i)) { icon = 'fa ' + icon; }
+      }
+    }
+    this._faIcon = icon;
+  }
 
-  // get faIcon() { return this._faIcon; }
+  get faIcon() { return this._faIcon; }
   // ngOnInit sanitizes faIcon on init
   ngOnInit() {
   }
